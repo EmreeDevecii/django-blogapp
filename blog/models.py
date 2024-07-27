45e4148c7d7133da47771fedfc1ac09e96152989
Emre Deveci <144159254+EmreeDevecii@users.noreply.github.com>
2024-07-27 17:48:54 +0300

    first commit

---
+++ blog/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+from django.utils.text import slugify
+
+
+# abc.com/category/beyaz-esya
+class Category(models.Model):
+    name = models.CharField(max_length=150)
+    slug = models.SlugField(null=False, blank=True, unique=True, db_index=True, editable=False)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+    
+
+#blogs/1.jpg
+#movies/5.jpg
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to="blogs")
+    description = models.TextField()
+    is_active = models.BooleanField(default=False)
+    is_home = models.BooleanField(default=False)
+    slug = models.SlugField(null=False, blank=True, unique=True, db_index=True, editable=False)
+    categories = models.ManyToManyField(Category, blank=True)
+
+    def __str__(self):
+        return f"{self.title}"
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+    
+
+
+
+
+# Create your models here.
